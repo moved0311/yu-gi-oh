39f9c25689c5951b5ff4a6a9135b3bdf6d945d2e
taiyi <taiyijiang@anuegroup.com.tw>
2023-02-27 16:57:31 +0800

    feat: fix mobile layout

--- src/components/SearchResult/index.tsx
+++ src/components/SearchResult/index.tsx
@@ -14,12 +14,14 @@ const SearchResult: FC<Props> = ({ data = [] }) => {
         Header: "",
         accessor: "id",
         width: "10%",
+        styles: { minWidth: "130px" },
         Cell: ({ value }: any) => <img alt={`${value}-img`} src={`${process.env.PUBLIC_URL}/assets/cards/${value}.jpg`} className="p-4" />,
       },
       {
         Header: "名稱",
         accessor: "name",
         width: "20%",
+        styles: { minWidth: "200px" },
         Cell: ({ row }: any) => {
           const name = row?.original?.name;
           const nameEnglish = row?.original?.name_en;
@@ -52,7 +54,7 @@ const SearchResult: FC<Props> = ({ data = [] }) => {
       {
         Header: "效果",
         accessor: "desc",
-        styles: { paddingLeft: "20px" },
+        styles: { paddingLeft: "20px", minWidth: "400px" },
         Cell: ({ value }: any) => {
           return <pre>{value}</pre>;
         },
@@ -66,13 +68,13 @@ const SearchResult: FC<Props> = ({ data = [] }) => {
   // let result = fuzzysort.single("query", "some string that contains my query.") || undefined;
   // {parse(fuzzysort.highlight(result, "<b>", "</b>") || "")}
 
-  console.log(data);
+  // console.log(data);
 
   return (
-    <>
+    <div className="w-full">
       {data.length}筆
       <Table data={data} columns={columns} />
-    </>
+    </div>
   );
 };
 
